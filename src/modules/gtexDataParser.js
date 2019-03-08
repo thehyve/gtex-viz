9d50acf6674a80e942b5fbe9ed9e3bd023df58ae
dnguyen <dnguyen@broadinstitute.org>
2019-03-08 13:57:35 -0500

    updating batch gene expr examples to work with v8

--- src/modules/gtexDataParser.js
+++ src/modules/gtexDataParser.js
@@ -4,8 +4,8 @@
  */
 "use strict";
 export function getGtexUrls(){
-    const host = 'https://gtexportal.org/rest/v1/';
-    // const host = 'http://local.gtex.org/rest/v1/';
+    // const host = 'https://gtexportal.org/rest/v1/';
+    const host = 'http://local.gtex.org/rest/v1/';
     return {
         // gene-eqtl visualizer specific
         singleTissueEqtl: host + 'association/singleTissueEqtl?format=json&datasetId=gtex_v8&gencodeId=',
